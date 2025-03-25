29edb74a21759a83546796af18f60323fc4b4591
alex <darkeitor6@outlook.com>
2025-03-24 23:26:10 -0500

    cookie fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,17 +7,16 @@ const router = Router()
 router.post('/guest', function handleGuestToken(req, res) {
     const { guest_name } = req.body
 
-    if (!guest_name || guest_name.length < 3 || guest_name > 30) {
+    if (!guest_name || guest_name.length < 3 || guest_name.length > 30) {
         return res.status(400).json({error: "El nombre debe tener entre 3 y 30 caracteres"})
     }
     const guest_id = randomUUID()
     try {
         const token = jwt.sign({ guest_id, guest_name }, process.env.SECRET_KEY)
-        console.log(process.env.NODE_ENV)
         res.cookie('token', token, { 
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Lax' 
+            sameSite: 'none' 
         })
         res.json({message: 'Login exitoso'})
     } catch (error) {
